2099bc73ce734c6f928db4cf0a1581d49746914e
implicitmutation <deast@google.com>
2024-07-25 12:47:08 -0400

    fix license headers

--- go/web-template/main_test.go
+++ go/web-template/main_test.go
@@ -16,10 +16,6 @@
   permissions and
   limitations under the License.
 */
-// Copyright 2023 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 package main
 
 import (
--- vue-vite/dev.nix
+++ vue-vite/dev.nix
@@ -1,3 +1,5 @@
+
+
 /*
   Copyright 2024 Google LLC
   
