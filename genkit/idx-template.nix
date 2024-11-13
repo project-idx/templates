4f3e0852af5b3de143a064f3539d99b528c963af
samphillips <samphillips@google.com>
2024-11-13 03:46:44 +0000

    Updates

--- genkit/idx-template.nix
+++ genkit/idx-template.nix
@@ -15,7 +15,7 @@ idx-template \
     cp -r ${./.idx}/. "$out/.idx/"
     cp -f ${./package.json} "$out/package.json"
     cp -f ${./package-lock.json} "$out/package-lock.json"
-    cp -f ${./server.ts} "$out/index.ts"
+    cp -f ${./index.ts} "$out/index.ts"
     cp -f ${./.gitignore} "$out/.gitignore"
     cp ${./README.md} "$out"/README.md
     ${if language == "go" then "env -C \"$out\" go mod init genkit" else "" }
