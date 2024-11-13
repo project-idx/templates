41deb1c3d1bf1bdf0d9729e032370c0b59c4fe3e
samphillips <samphillips@google.com>
2024-11-13 03:37:55 +0000

    Updates

--- genkit/idx-template.nix
+++ genkit/idx-template.nix
@@ -12,7 +12,6 @@ idx-template \
   bootstrap = ''
     mkdir "$out"
     mkdir "$out"/.idx
-    cp ${./dev.nix} "$out"/.idx/dev.nix
     cp ${./README.md} "$out"/README.md
     ${if language == "go" then "env -C \"$out\" go mod init genkit" else "" }
     chmod -R u+w "$out"
