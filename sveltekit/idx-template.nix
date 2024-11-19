bd35ddb5490b97762b935979ee14ed4df9727960
Rody Davis <31253215+rodydavis@users.noreply.github.com>
2024-11-18 21:51:22 -0300

    Update idx-template.nix

--- sveltekit/idx-template.nix
+++ sveltekit/idx-template.nix
@@ -1,5 +1,5 @@
 # /opt/nix/store/qgx9xsmmwjkflakfcxz52ncmihhr3mg9-idx-env-usr-target/bin/idx-template ~/Monospace/workspace/nix_templates/sveltekit --output-dir ~ --workspace-name foo -a '{"template": "default", "types": "checkjs", "prettier": "true", "eslint": "true", "playwright": "true", "vitest": "true"}'
-{ pkgs, template, types, eslint ? false, prettier ? false, playwright ? false, vitest ? false, ... }: {
+{ pkgs, template, types, prettier ? false, eslint ? false, vitest ? false, playwright ? false, tailwindcss? false, drizzle ? false, lucia ? false, mdsvex ? false, paraglide ? false, storybook ? false, ... }: {
     packages = [
       pkgs.nodejs_20
     ];
@@ -11,7 +11,7 @@
 
       # install every time so that we get latest version of `create-svelte`
       npm install
-      node init.js name="$WS_NAME" --template=${template} --types=${types} prettier=${toString prettier} eslint=${toString eslint} playwright=${toString playwright} vitest=${toString vitest}
+      node init.js "$WS_NAME" --template=${template} --types=${types} prettier=${toString prettier} eslint=${toString eslint} vitest=${toString vitest} playwright=${toString playwright} tailwindcss=${toString tailwindcss} drizzle=${toString drizzle} lucia=${toString lucia} mdsvex=${toString mdsvex} paraglide=${toString paraglide} storybook=${toString storybook}
 
       chmod -R +w "$WS_NAME"
       cp -rf "$WS_NAME" "$out"
