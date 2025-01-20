05902441ff603b6fddb24d92bb5a5efe079763e8
Nikolas Burk <burk@prisma.io>
2025-01-20 13:39:34 -0700

    add prisma postgres as another option

---
+++ prisma/app-ppg/.idx/dev.nix
@@ -0,0 +1,45 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-24.05"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.nodejs
+    pkgs.openssl
+  ];
+
+  # Sets environment variables in the workspace
+  env = {
+    DATABASE_URL="";
+    PULSE_API_KEY="";
+  };
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "Prisma.prisma"
+    ];
+
+    # Enable previews
+    previews = {
+      enable = true;
+      previews = {
+      };
+    };
+
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        npm-install = "npm i --no-audit --prefer-offline";
+        # init-db = "npx prisma@6.3.0-dev.9 init --db"; # auto-creation of DBs will be enabled soon
+        default.openFiles = [ "README.md" ];
+      };
+      # Runs when the workspace is (re)started
+      onStart = {
+        setup-instructions = "echo \"Follow the instructions in the README to set up your Prisma Postgres database for this project.\"";
+      };
+    };
+  };
+}
