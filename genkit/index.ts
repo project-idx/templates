44751714c5e86bb8459de93872e22768346cce6b
deast <deast@google.com>
2025-02-21 01:34:22 +0000

    genkit 1.0

--- genkit/index.ts
+++ genkit/index.ts
@@ -1,7 +1,7 @@
-import { genkit, z } from 'genkit';
+import { genkit, z } from 'genkit/beta';
 import { googleAI, gemini20Flash } from '@genkit-ai/googleai';
-import { messages } from './initial-data';
-import { HNItemSchema, HNItemsSchema, HNPageSchema, PageSchema, UserSchema } from './schema';
+import { HNItemsSchema } from './schema';
+import { createInterface } from "node:readline/promises";
 
 export const ai = genkit({
 	plugins: [googleAI()],
@@ -19,24 +19,23 @@ export const weatherTool = ai.defineTool({
 	return `The weather in ${location} is sunny`
 });
 
-export const hackerNewsTool = ai.defineTool<z.ZodVoid, z.ZodAny>({
+export const hackerNewsTool = ai.defineTool<z.ZodVoid, typeof HNItemsSchema>({
 	name: 'getHackerNewsByPage',
 	description: 'Gets the json page number',
 	inputSchema: z.void(),
-	outputSchema: z.any(),
+	outputSchema: HNItemsSchema,
 }, async () => {
 	const response = await fetch(`https://api.hnpwa.com/v0/news/1.json`)
-	const jsonData = await response.json() as typeof HNItemsSchema;
-	// const items = HNItemsSchema.safeParse(jsonData)
-	// console.log('--------------')
-	// console.log(`
-	
-	// 	ITEMS
-	
-	// `)
-	// console.log('--------------')
-	// console.log(jsonData)
-	return { page: 1, items: jsonData };
+	const jsonData = await response.json();
+	const parseMeMaybe = HNItemsSchema.safeParse(jsonData)
+	if(parseMeMaybe.success) {
+		return parseMeMaybe.data
+	}
+	const errorDetails = parseMeMaybe.error.errors
+  .map((error) => `Path: ${error.path.join('.')} - ${error.message}`)
+  .join('\n');
+	const stack = `STACK: ${parseMeMaybe.error.stack}`;
+	throw new Error(`Zod Validation Error:\n${errorDetails}\n\n${stack}`);
 });
 
 export const hackerNewsSummaryTool = ai.defineTool<z.ZodVoid, z.ZodString>({
@@ -45,46 +44,26 @@ export const hackerNewsSummaryTool = ai.defineTool<z.ZodVoid, z.ZodString>({
 	inputSchema: z.void(),
 	outputSchema: z.string(),
 }, async () => {
-	const pageResult = await hackerNewsTool()
-	const result = await ai.generate<z.ZodString>(`Summarize the following new on page ${pageResult.page}: ${JSON.stringify(pageResult.items)}`)
-	return result.text
+	const items = await hackerNewsTool()
+	const result = await ai.generate<z.ZodString>(`Summarize the following news: ${JSON.stringify(items)}`)
+	return result.text;
 })
 
-const chatPrompt = ai.definePrompt({
-  name: 'chat',
+const chat = ai.chat({
 	model: gemini20Flash,
-	system: 'You are a helpful assistant.',
-	prompt: async ({ text, user }) => `
-		Help this user with their task below.
-		The user ${user?.fullName} says: ${text}
-	`,
-  input: {
-    schema: z.object({
-      text: z.string().describe('A chat message from a user'),
-			// Schemas can be imported as well
-			user: UserSchema.optional(),
-    })
-  },
+	system: 'You are a helpful assistant. If the user asks you what you can do, respond with your tools and creative uses.',
 	tools: [weatherTool, hackerNewsSummaryTool],
 });
 
 
-const result = await chatPrompt({
-	text: "Summarize the first page of Hacker News",
-	user: {
-		username: 'davideast',
-		fullName: 'David East'
-	}
-});
+async function main() {
+  console.log("You're chatting with Gemini. Ctrl-C to quit.\n");
+  const readline = createInterface(process.stdin, process.stdout);
+  while (true) {
+    const userInput = await readline.question("> ");
+		const result = await chat.send(userInput)
+    console.log(result.text);
+  }
+}
 
-console.log(`
-
------------
-
-// SUMMARY
-
------------
-
-${result.message?.text}
-
-`)
+main();
