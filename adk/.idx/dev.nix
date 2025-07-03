ae34093bde16a3ab7494dbd7abfdb3e308d76229
estherwasatester <73840310+estherwasatester@users.noreply.github.com>
2025-07-03 17:56:10 +0100

    feat: Add ADK Python Quickstart template with Nix (#110)
    * feat: adk template
    
    * fix: folder name
    
    * fix: env vars
    
    * fix: env file

---
+++ adk/.idx/dev.nix
@@ -0,0 +1,39 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-24.05"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [ pkgs.python3 ];
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [ "ms-python.python" ];
+    workspace = {
+      # Runs when a workspace is first created with this `dev.nix` file
+      onCreate = {
+        install =
+          "python -m venv .venv && source .venv/bin/activate && pip install -r requirements.txt";
+        # Open editors for the following files by default, if they exist:
+        default.openFiles = [ "./multi_tool_agent/agent.py"];
+      };
+      # Runs each time the workspace is (re)started
+      onStart = {
+        install =
+          "python -m venv .venv && source .venv/bin/activate && pip install -r requirements.txt";
+        # Open editors for the following files by default, if they exist:
+        default.openFiles = [".env.local" "./multi_tool_agent/agent.py"];
+      };
+    };
+    # Enable previews and customize configuration
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = [ "./devserver.sh" ];
+          env = { PORT = "$PORT"; };
+          manager = "web";
+        };
+      };
+    };
+  };
+}
---
+++ adk/README.md
@@ -0,0 +1,25 @@
+# Agent Developer Kit Quickstart
+
+To get started, your agent needs to make secure calls to an external LLM service.
+
+Set up your GOOGLE_API_KEY in your [.env](./multi_tool_agent/.env.local) file. Get your Gemini API key from https://g.co/ai/idxGetGeminiKey.
+
+To use Google Cloud Vertex AI follow the [adk-docs]](https://google.github.io/adk-docs/get-started/quickstart/#set-up-the-model).
+
+You can find more information about the basics of using the ADK framework in the [Agent Development Kit Documentation](https://google.github.io/adk-docs/get-started/quickstart).
+
+## Try it out!
+<a href="https://idx.google.com/new?template=https://github.com/firebase-studio/templates/tree/main/adk">
+  <picture>
+    <source
+      media="(prefers-color-scheme: dark)"
+      srcset="https://cdn.idx.dev/btn/open_dark_32.svg">
+    <source
+      media="(prefers-color-scheme: light)"
+      srcset="https://cdn.idx.dev/btn/open_light_32.svg">
+    <img
+      height="32"
+      alt="Open in IDX"
+      src="https://cdn.idx.dev/btn/open_purple_32.svg">
+  </picture>
+</a>
