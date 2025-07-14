29f755bf618dd9324d3e8eab4a975574f8c1b3d1
Rody Davis <rody.davis.jr@gmail.com>
2025-07-14 16:01:00 -0700

    remove symlink for now

--- lit-vite/idx-template.nix
+++ lit-vite/idx-template.nix
@@ -14,7 +14,7 @@
     chmod -R u+w "$out"
     mkdir -p "$out/.idx"
     cp ./.idx/airules.md "$out/.idx"
-    cp ./gemini.md "$out"
+    # cp ./gemini.md "$out"
     
     cd "$out"; npm install --package-lock-only --ignore-scripts
   '';
