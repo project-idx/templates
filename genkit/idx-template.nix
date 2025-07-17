0a3c9bf7b3dc61c1fa085ef71b20e5585077b2f7
Rody Davis <rody.davis.jr@gmail.com>
2025-07-16 21:25:10 -0700

    Update idx-template.nix

--- genkit/idx-template.nix
+++ genkit/idx-template.nix
@@ -23,7 +23,6 @@ idx-template \
     mkdir -p "$out/.idx"
     cp -rf ${./.idx/airules.md} "$out/.idx/airules.md"
     cp -rf "$out/.idx/airules.md" "$out/GEMINI.md"
-    chmod -R u+w "$out"
     
     chmod -R u+w "$out"
   '';
