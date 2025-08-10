87526548bebf295fe435f35c19a881f889de7da5
alan bount <alan@zeroasterisk.com>
2025-08-10 12:48:55 -0400

    fix-paths

--- adk/idx-template.nix
+++ adk/idx-template.nix
@@ -8,17 +8,17 @@
     mkdir "$out"/.gemini
     mkdir "$out"/multi_tool_agent
     cp -r ${./.idx}/. "$out/.idx/"
+    cp -r ${./.gemini}/. "$out/.gemini"
+    cp -r ${./multi_tool_agent}/. "$out/multi_tool_agent"
+    cp -f ${./multi_tool_agent}/.env.local "$out/multi_tool_agent/.env.local"
+    link -s "$out/multi_tool_agent/.env.local" "$out/.env.local"
     cp -f ${./.gitignore} "$out/.gitignore"
     cp -f ${./requirements.txt} "$out/requirements.txt"
     cp -f ${./llms-full.txt} "$out/llms-full.txt"
-    cp -f ${./.gemini/settings.json} "$out/settings.json"
-    cp -f ${./multi_tool_agent} "$out/multi_tool_agent"
-
-    cp ${./README_TEMPLATE.md} "$out"/README.md
-
-    cp ${./.idx/airules.md} "$out/.idx/airules.md"
-    cp ${./.idx/airules.md} "$out/GEMINI.md"
-    cp ${./AGENTS.md} "$out/AGENTS.md"
+    cp -f ${./README_TEMPLATE.md} "$out"/README.md
+    cp -f ${./.idx/airules.md} "$out/.idx/airules.md"
+    cp -f ${./.idx/airules.md} "$out/GEMINI.md"
+    cp -f ${./AGENTS.md} "$out/AGENTS.md"
     chmod -R u+w "$out"
   '';
 }
